4caeef4084bdd8ce84946a948e16bcfc3f3fe549
iscream <210@qq.com>
2018-11-04 13:28:36 +0800

    SpringCloudTempleV2.0

--- eureka_client_video_user/src/main/java/com/kaige123/daomu/eureka_client_video_user/EurekaClientVideoUserApplication.java
+++ eureka_client_video_user/src/main/java/com/kaige123/daomu/eureka_client_video_user/EurekaClientVideoUserApplication.java
@@ -4,9 +4,13 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableEurekaClient
+@EnableFeignClients
+@EnableHystrix
 @MapperScan("com.kaige123.daomu.eureka_client_video_user.DAO.Mapper")
 public class EurekaClientVideoUserApplication {
 
--- eureka_client_video_user/src/main/java/com/kaige123/daomu/eureka_client_video_user/test/test.java
+++ eureka_client_video_user/src/main/java/com/kaige123/daomu/eureka_client_video_user/test/test.java
@@ -1,13 +1,37 @@
 package com.kaige123.daomu.eureka_client_video_user.test;
 
+import com.kaige123.daomu.eureka_client_video_user.FeignWord.test.testInvoke;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class test {
-    @GetMapping("/test")
+
+    @GetMapping("/UserTest")
     public String test() {
-        System.out.println("请求成功");
-        return "请求成功";
+        System.out.println("USER test 请求成功");
+        return "USER test 请求成功";
+    }
+
+    @Autowired
+    testInvoke testInvokevar;
+
+    //    feign转发远程调用服务
+    @GetMapping("/FeginTest")
+    public String FeginTest() {
+        System.out.println("FeginTest 调用成功");
+        return "FeginTest调起结果: " + testInvokevar.test();
+    }
+
+    @HystrixCommand(fallbackMethod = "ServerExceptionMsg")
+    @GetMapping("/ErrorTest")
+    public String ErrorTest() {
+        throw new RuntimeException();
+    }
+
+    public String ServerExceptionMsg() {
+        return "服务挂了,正在救急,请稍后...";
     }
 }
